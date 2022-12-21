e583897832498917c288fcdd273d50d42430e88f
Akmaljon Azimov <96201968+AkmaljonCoder@users.noreply.github.com>
2022-12-21 17:43:55 +0500

    akmal

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Button, CloseIc, Input, InpWrapper, LogoImg, Title, Wrapper } from './style'
 
 import Close from '../../Assets/close.svg'
 import Logo from '../../Assets/logo.png'
 
 const Order = ({order,setOrder}) => {
+    const [name, setname] = useState('');
+    const [number, setnumber] = useState('');
+    const [guest, setguest] = useState('');
+    const [time, settime] = useState('');
 
-
+    const SendFunc = () => {
+        setname('');
+        setnumber('');
+        setguest('');
+        settime('');
+        setOrder(false);
+    }
 
   return (
     <Wrapper>
@@ -14,13 +24,13 @@ const Order = ({order,setOrder}) => {
             <div className="card">
                 <LogoImg src={Logo} />
                 <Title>Забронировать столик</Title>
-                <Input placeholder='Имя'/>
-                <Input placeholder='Телефон'/>
+                <Input onChange={(e)=>setname(e.target.value)} value={name} placeholder='Имя'/>
+                <Input onChange={(e)=>setnumber(e.target.value)} value={number} placeholder='Телефон'/>
                 <InpWrapper>
-                    <input placeholder='Гостей' type="text" className="input" />
-                    <input placeholder='Время' type="text" className="input" />
+                    <input value={guest} onChange={(e)=>setguest(e.target.value)} placeholder='Гостей' className="input" />
+                    <input type={'time'} onChange={(e)=>settime(e.target.value)} value={time} placeholder='Время' className="input" />
                 </InpWrapper>
-                <Button>Забронировать</Button>
+                <Button onClick={SendFunc}>Забронировать</Button>
                 <CloseIc
                 onClick={()=>setOrder(false)} 
                 src={Close} alt="" className="close"
